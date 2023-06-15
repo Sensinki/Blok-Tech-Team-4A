3a36d4d8f3869929f1c77a68c6ba58d4927bf672
dinhs-frontend <haisang7@live.nl>
2023-06-15 12:27:56 +0200

    geupdate feature werkt

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 const bodyParser = require('body-parser');
-const {engine} = require('express-handlebars');
+const { engine } = require('express-handlebars');
 
 require('dotenv').config();
 
@@ -14,8 +15,7 @@ const url = process.env.DB_CONNECTION_STRING;
 
 let result;
 
-
-async function connectToMongoDB() {
+async function connectToMongoDB(index) {
   try {
     const client = new MongoClient(url);
     await client.connect();
@@ -24,28 +24,33 @@ async function connectToMongoDB() {
 
     const db = client.db('dataTeam');
     const collection = db.collection('gamesData');
+
+    const likedUsers = [
+      { name: 'Abdel', image: '/css/img/profiles/abdel.jpeg', game: 'Minecraft' },
+      { name: 'Karen', image: '/css/img/profiles/Sang.cv.jpg', game: 'Valorand' },
+      { name: 'Angelo', image: '/css/img/profiles/angelo.jpeg', game: 'LOL' },
+    ];
+
+    const selectedUser = likedUsers[index];
+    await collection.insertOne(selectedUser);
     result = await collection.find({}).toArray();
 
     client.close();
-    console.log('Verbinding met de database');
+    console.log('Verbinding met de database gesloten.');
   } catch (error) {
-    console.error('Er is een fout opgetreden bij het verbinden met de database:', error
-    );
+    console.error('Er is een fout opgetreden bij het verbinden met de database:', error);
   }
 }
 
-
-// Roep de functie aan om verbinding te maken met de database.
-connectToMongoDB();
-
+// Roep de functie aan om verbinding te maken met de database en een specifiek likedUser-object te sturen.
+connectToMongoDB(1); // Stuurt het tweede likedUser-object naar MongoDB (index 1).
 
 // --------------------------------------------------------------//
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/static'));
 
-// app.engine('handlebars', match());
-app.engine('handlebars', engine({defaultLayout: 'main'}));
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', 'views');
 
@@ -53,18 +58,25 @@ app.get('/match', match);
 app.get('/like', like);
 
 function match(req, res) {
-    res.render('match');
+  res.render('match');
 }
 
 function like(req, res) {
-    res.render('like');
+  res.render('like');
 }
 
+let likedUserProfile;
+
 app.post('/like', (req, res) => {
-    const likedUser = req.body.like;
-    res.render('match', {likedUser});
+  const likedUser = req.body.like;
+  likedUserProfile = likedUser;
+  res.render('like', { likedUser });
+});
+
+app.get('/like', (req, res) => {
+  res.render('like', { likedUserProfile });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
--- static/script/script.js
+++ static/script/script.js
@@ -1,32 +1,41 @@
+/* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
+// profielen //
 const profiles = [
-    { name: 'Karen', image: '/Blok-Tech-Team-4A/static/css/img/profiles/Sang.cv.jpg' },
-    { name: 'Abdel', image: '/Blok-Tech-Team-4A/static/css/img/profiles/abdel.jpeg' },
-    { name: 'angelo', image: '/Blok-Tech-Team-4A/static/css/img/profiles/angelo.jpeg' },
-    // Voeg hier meer profielen toe
-  ];
-  
-  let currentProfileIndex = 0;
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    const dislikeButton = document.querySelector('.DisLikeButton');
-    
-    dislikeButton.addEventListener('click', loadNextProfile);
-    
-    function loadNextProfile() {
-      currentProfileIndex++;
-      if (currentProfileIndex >= profiles.length) {
-        currentProfileIndex = 0;
-      }
-    
-      const currentProfile = profiles[currentProfileIndex];
-    
-      const profileImage = document.querySelector('.userimg');
-      profileImage.style.backgroundImage = `url(${currentProfile.image})`;
-    
-      const profileName = document.querySelector('h3');
-      profileName.textContent = currentProfile.name;
+  {name: 'Karen', image: '/css/img/profiles/Sang.cv.jpg', game: 'Valorand'},
+  {name: 'Abdel', image: '/css/img/profiles/abdel.jpeg', game: 'Minecraft'},
+  {name: 'Angelo', image: '/css/img/profiles/angelo.jpeg', game: 'LOL'},
+
+];
+
+let currentProfileIndex = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dislikeButton = document.querySelector('.DisLikeButton');
+
+  dislikeButton.addEventListener('click', loadNextProfile);
+
+  function loadNextProfile() {
+    currentProfileIndex++;
+    if (currentProfileIndex >= profiles.length) {
+      currentProfileIndex = 0;
     }
-  });
-  
-  
+
+    const currentProfile = profiles[currentProfileIndex];
+
+    const profileImage = document.querySelector('.userimg');
+    profileImage.style.backgroundImage = `url(${currentProfile.image})`;
+
+    const profileName = document.querySelector('h3');
+    profileName.textContent = currentProfile.name;
+
+    const profileGame = document.querySelector('p');
+    profileGame.textContent = currentProfile.game;
+
+    const likeButton = document.querySelector('.LikeButton');
+
+    likeButton.value = JSON.stringify(currentProfile); // Converteer het huidige profiel naar een JSON-string en stel het in als de waarde van de like-knop
+  }
+});
+
+// -------------------------------------------------------- //
