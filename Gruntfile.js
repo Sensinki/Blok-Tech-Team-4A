babb86d6b7cf3ba492737b5a1f72d99a1111b247
senanurkorkutan <snnrkrk1@gmail.com>
2023-06-22 23:33:27 +0200

    signup css en css klusjes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,73 +1,88 @@
+/* eslint-disable indent */
 module.exports = function (grunt) {
-  grunt.loadNpmTasks("grunt-contrib-uglify");
-  grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-imagemin");
 
-  var JavaScriptObfuscator = require("javascript-obfuscator");
+    var JavaScriptObfuscator = require("javascript-obfuscator");
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
 
-    uglify: {
-      target: {
-        files: [
-          {
-            expand: true,
-            cwd: "public/js",
-            src: ["*.js"],
-            dest: "build/js",
-            ext: ".min.js",
-          },
-        ],
-      },
-    },
-
-    cssmin: {
-      target: {
-        files: [
-          {
-            expand: true,
-            cwd: "public/css",
-            src: ["*.css"],
-            dest: "build/css",
-            ext: ".min.css",
-          },
-        ],
-      },
-    },
-
-    watch: {
-      scripts: {
-        files: ["public/js/*.js"],
-        tasks: ["uglify", "obfuscate"],
-        options: {
-          livereload: true,
+        uglify: {
+            target: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "public/js",
+                        src: ["*.js"],
+                        dest: "build/js",
+                        ext: ".min.js",
+                    },
+                ],
+            },
         },
-      },
-      styles: {
-        files: ["public/css/*.css"],
-        tasks: ["cssmin"],
-        options: {
-          livereload: true,
+
+        cssmin: {
+            target: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "public/css",
+                        src: ["*.css"],
+                        dest: "build/css",
+                        ext: ".min.css",
+                    },
+                ],
+            },
         },
-      },
-    },
-  });
 
-  grunt.registerTask("obfuscate", function () {
-    var files = grunt.file.expand("build/js/*.min.js");
+        imagemin: {
+            dynamic: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "static/images",
+                        src: ["*/.{jpg,jpeg,png,svg,gif}"],
+                        dest: "build/images",
+                    },
+                ],
+            },
+        },
 
-    files.forEach(function (file) {
-      var sourceCode = grunt.file.read(file);
-      var obfuscationResult = JavaScriptObfuscator.obfuscate(sourceCode);
-
-      grunt.file.write(file, obfuscationResult.getObfuscatedCode());
+        watch: {
+            scripts: {
+                files: ["public/js/*.js"],
+                tasks: ["uglify", "obfuscate"],
+                options: {
+                    livereload: true,
+                },
+            },
+            styles: {
+                files: ["public/css/*.css"],
+                tasks: ["cssmin"],
+                options: {
+                    livereload: true,
+                },
+            },
+        },
     });
 
-    grunt.log.writeln("JavaScript files obfuscated.");
-  });
+    grunt.registerTask("obfuscate", function () {
+        var files = grunt.file.expand("build/js/*.min.js");
 
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-contrib-cssmin");
+        files.forEach(function (file) {
+            var sourceCode = grunt.file.read(file);
+            var obfuscationResult = JavaScriptObfuscator.obfuscate(sourceCode);
 
-  grunt.registerTask("default", ["uglify", "cssmin", "obfuscate", "watch"]);
+            grunt.file.write(file, obfuscationResult.getObfuscatedCode());
+        });
+
+        grunt.log.writeln("JavaScript files obfuscated.");
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+
+    grunt.registerTask("default", ["uglify", "cssmin", "obfuscate", "imagemin", "watch"]);
 };
